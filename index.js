9576719ae7acf20b2eb67098cf9b094dab590592
Mohamed Marghoub <mohamedmarghoub0726@gmail.com>
2026-06-13 11:51:46 +0100

    Declare variables for the calculator logic

---
+++ index.js
@@ -0,0 +1,14 @@
+const calcDisplay = document.querySelector(".calculator__display");
+
+const digitBtns = document.querySelectorAll(".btn_type_digit");
+const operatorBtns = document.querySelectorAll(".btn_type_operator");
+
+const evaluateBtn = document.querySelector(".btn_type_evaluate");
+
+const clearBtn = document.querySelector(".btn_type_clear");
+
+let isEvaluated = false;
+
+let strFirstNumber = "";
+let strSecondNumber = "";
+let operator = "";
